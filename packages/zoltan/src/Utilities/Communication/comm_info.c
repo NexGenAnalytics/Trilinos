446277ac9b1bb7bb23c7577e7e84b5aae7911248
Arkadiusz Szczepkowicz <arek.szczepkowicz@ng-analytics.com>
2023-08-08 18:05:02 +0200

    #42: Replace usage of MPI_COMM_WORLD in src/Utilities

--- packages/zoltan/src/Utilities/Communication/comm_info.c
+++ packages/zoltan/src/Utilities/Communication/comm_info.c
@@ -90,7 +90,7 @@ int i, j, k, my_proc;
 
   /* Check input parameters */
   if (!plan) {
-    MPI_Comm_rank(MPI_COMM_WORLD, &my_proc);
+    MPI_Comm_rank(MPI_Comm_Default(), &my_proc);
     ZOLTAN_COMM_ERROR("Communication plan = NULL", yo, my_proc);
     return ZOLTAN_FATAL;
   }
