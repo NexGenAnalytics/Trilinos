2b8a335e99a4b175027e8509304905917df19726
Caleb Schilly <cwschilly@gmail.com>
2023-08-09 18:19:50 -0400

    #87: fix build.sh

--- nga-ci/build.sh
+++ nga-ci/build.sh
@@ -10,48 +10,47 @@ export MPICXX_DIR=$(which mpicxx)
 export MPIF90_DIR=$(which mpif90)
 export MPIRUN_DIR=$(which mpirun)
 
-cmake -G "Ninja" \
-    -D CMAKE_GENERATOR STRING : Ninja \
-    -D Trilinos_PARALLEL_LINK_JOBS_LIMIT STRING : 2 \
-    -D Trilinos_ENABLE_ALL_PACKAGES BOOL : ON \
-    -D Trilinos_ENABLE_ALL_OPTIONAL_PACKAGES BOOL : ON \
-    -D Trilinos_ALLOW_NO_PACKAGES BOOL : ON \
-    -D Trilinos_DISABLE_ENABLED_FORWARD_DEP_PACKAGES BOOL : ON \
-    -D Trilinos_IGNORE_MISSING_EXTRA_REPOSITORIES BOOL : ON \
-    -D Trilinos_ENABLE_TESTS BOOL : ON \
-    -D Trilinos_TEST_CATEGORIES STRING : BASIC \
-    -D Trilinos_ENABLE_ALL_FORWARD_DEP_PACKAGES BOOL : ON \
+cmake -G "${CMAKE_GENERATOR:-Ninja}" \
+    -D Trilinos_PARALLEL_LINK_JOBS_LIMIT=2 \
+    -D Trilinos_ENABLE_ALL_PACKAGES=ON \
+    -D Trilinos_ENABLE_ALL_OPTIONAL_PACKAGES=ON \
+    -D Trilinos_ALLOW_NO_PACKAGES=ON \
+    -D Trilinos_DISABLE_ENABLED_FORWARD_DEP_PACKAGES=ON \
+    -D Trilinos_IGNORE_MISSING_EXTRA_REPOSITORIES=ON \
+    -D Trilinos_ENABLE_TESTS=ON \
+    -D Trilinos_TEST_CATEGORIES=BASIC \
+    -D Trilinos_ENABLE_ALL_FORWARD_DEP_PACKAGES=ON \
     \
-    -D TPL_ENABLE_Matio    BOOL : OFF \
-    -D TPL_ENABLE_X11      BOOL : OFF \
-    -D TPL_ENABLE_Pthread  BOOL : OFF \
-    -D TPL_ENABLE_BLAS     BOOL : ON \
-    -D TPL_ENABLE_LAPACK   BOOL : ON \
-    -D TPL_ENABLE_Boost    BOOL : OFF \
-    -D TPL_ENABLE_BoostLib BOOL : OFF \
-    -D TPL_ENABLE_ParMETIS BOOL : OFF \
-    -D TPL_ENABLE_Zlib     BOOL : OFF \
-    -D TPL_ENABLE_HDF5     BOOL : OFF \
-    -D TPL_ENABLE_Netcdf   BOOL : OFF \
-    -D TPL_ENABLE_SuperLU  BOOL : OFF \
-    -D TPL_ENABLE_Scotch   BOOL : OFF \
+    -D TPL_ENABLE_Matio=OFF \
+    -D TPL_ENABLE_X11=OFF \
+    -D TPL_ENABLE_Pthread=OFF \
+    -D TPL_ENABLE_BLAS=ON \
+    -D TPL_ENABLE_LAPACK=ON \
+    -D TPL_ENABLE_Boost=OFF \
+    -D TPL_ENABLE_BoostLib=OFF \
+    -D TPL_ENABLE_ParMETIS=OFF \
+    -D TPL_ENABLE_Zlib=OFF \
+    -D TPL_ENABLE_HDF5=OFF \
+    -D TPL_ENABLE_Netcdf=OFF \
+    -D TPL_ENABLE_SuperLU=OFF \
+    -D TPL_ENABLE_Scotch=OFF \
     \
-    -D CMAKE_C_COMPILER       FILEPATH : ${MPICC_DIR} \
-    -D CMAKE_CXX_COMPILER     FILEPATH : ${MPICXX_DIR} \
-    -D CMAKE_Fortran_COMPILER FILEPATH : ${MPIF90_DIR} \
-    -D TPL_ENABLE_MPI         BOOL     : ON \
-    -D MPI_EXEC               FILEPATH : ${MPIRUN_DIR} \
+    -D CMAKE_C_COMPILER=${MPICC_DIR} \
+    -D CMAKE_CXX_COMPILER=${MPICXX_DIR} \
+    -D CMAKE_Fortran_COMPILER=${MPIF90_DIR} \
+    -D TPL_ENABLE_MPI=ON \
+    -D MPI_EXEC=${MPIRUN_DIR} \
     \
-    -D Trilinos_ENABLE_Rythmos BOOL : OFF \
-    -D Trilinos_ENABLE_Pike    BOOL : OFF \
-    -D Trilinos_ENABLE_Komplex BOOL : OFF \
-    -D Trilinos_ENABLE_TriKota BOOL : OFF \
-    -D Trilinos_ENABLE_Moertel BOOL : OFF \
-    -D Trilinos_ENABLE_Domi    BOOL : OFF \
-    -D Trilinos_ENABLE_FEI     BOOL : OFF \
+    -D Trilinos_ENABLE_Rythmos=OFF \
+    -D Trilinos_ENABLE_Pike=OFF \
+    -D Trilinos_ENABLE_Komplex=OFF \
+    -D Trilinos_ENABLE_TriKota=OFF \
+    -D Trilinos_ENABLE_Moertel=OFF \
+    -D Trilinos_ENABLE_Domi=OFF \
+    -D Trilinos_ENABLE_FEI=OFF \
     \
-    -D Trilinos_ENABLE_PyTrilinos BOOL : OFF \
+    -D Trilinos_ENABLE_PyTrilinos=OFF \
     \
-    -D Trilinos_ENABLE_Epetra BOOL : OFF \
+    -D Trilinos_ENABLE_Epetra=OFF \
     -S /opt/src/Trilinos -B /opt/build/Trilinos
-ninja -j3
+ninja -j6
