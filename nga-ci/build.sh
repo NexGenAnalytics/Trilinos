ecafee2b869a9ec1e125e69eb1770c9672fb99bd
Caleb Schilly <cwschilly@gmail.com>
2023-08-10 18:54:06 -0400

    #87: remove all TPLs

--- nga-ci/build.sh
+++ nga-ci/build.sh
@@ -7,10 +7,10 @@ spack env activate trilinos
 
 spack find -p
 
-export MPICC_DIR=$(which mpicc)
-export MPICXX_DIR=$(which mpicxx)
-export MPIF90_DIR=$(which mpif90)
-export MPIRUN_DIR=$(which mpirun)
+export MPICC=$(which mpicc)
+export MPICXX=$(which mpicxx)
+export MPIF90=$(which mpif90)
+export MPIRUN=$(which mpirun)
 
 export BLAS_ROOT="/opt/spack/opt/spack/linux-ubuntu22.04-x86_64_v3/gcc-11.4.0/openblas-0.3.23-bwv7xuj5t72zlgxhiq4wz3nyb35b2two"
 export LAPACK_ROOT="/opt/spack/opt/spack/linux-ubuntu22.04-x86_64_v3/gcc-11.4.0/openblas-0.3.23-bwv7xuj5t72zlgxhiq4wz3nyb35b2two"
@@ -27,9 +27,9 @@ cmake -G "${CMAKE_GENERATOR:-Ninja}" \
     -D Trilinos_TEST_CATEGORIES=BASIC \
     -D Trilinos_ENABLE_ALL_FORWARD_DEP_PACKAGES=ON \
     \
-    -D TPL_ENABLE_BLAS=ON \
+    -D TPL_ENABLE_BLAS=OFF \
     -D BLAS_LIBRARY_DIRS="${BLAS_ROOT}/lib" \
-    -D TPL_ENABLE_LAPACK=ON \
+    -D TPL_ENABLE_LAPACK=OFF \
     -D LAPACK_LIBRARY_DIRS="${LAPACK_ROOT}/lib" \
     \
     -D TPL_ENABLE_Matio=OFF \
@@ -44,11 +44,11 @@ cmake -G "${CMAKE_GENERATOR:-Ninja}" \
     -D TPL_ENABLE_SuperLU=OFF \
     -D TPL_ENABLE_Scotch=OFF \
     \
-    -D CMAKE_C_COMPILER=${MPICC_DIR} \
-    -D CMAKE_CXX_COMPILER=${MPICXX_DIR} \
-    -D CMAKE_Fortran_COMPILER=${MPIF90_DIR} \
+    -D CMAKE_C_COMPILER=${MPICC} \
+    -D CMAKE_CXX_COMPILER=${MPICXX} \
+    -D CMAKE_Fortran_COMPILER=${MPIF90} \
     -D TPL_ENABLE_MPI=ON \
-    -D MPI_EXEC=${MPIRUN_DIR} \
+    -D MPI_EXEC=${MPIRUN} \
     \
     -D Trilinos_ENABLE_Rythmos=OFF \
     -D Trilinos_ENABLE_Pike=OFF \
