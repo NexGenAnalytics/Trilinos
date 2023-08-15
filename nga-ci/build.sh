9dff45d0c12e132e6e58e44ea7dfb257659fd662
Caleb Schilly <cwschilly@gmail.com>
2023-08-15 13:48:34 -0400

    #87: fix CI failures

--- nga-ci/build.sh
+++ nga-ci/build.sh
@@ -9,16 +9,20 @@ cd /opt/build/Trilinos
 
 spack find -p
 
-export MPICC=$(which mpicc)
-export MPICXX=$(which mpicxx)
-export MPIF90=$(which mpif90)
-export MPIRUN=$(which mpirun)
+export MPI_ROOT="/opt/spack/opt/spack/linux-ubuntu22.04-x86_64_v3/gcc-11.4.0/openmpi-4.1.5-qjkxgt6ffv6shm6rgttbnwuerhchufq7"
+
+export MPICC="${MPI_ROOT}/bin/mpicc"
+export MPICXX="${MPI_ROOT}/bin/mpicxx"
+export MPIF90="${MPI_ROOT}/bin/mpif90"
+export MPIRUN="${MPI_ROOT}/bin/mpirun"
 
 export BLAS_ROOT="/opt/spack/opt/spack/linux-ubuntu22.04-x86_64_v3/gcc-11.4.0/openblas-0.3.23-bwv7xuj5t72zlgxhiq4wz3nyb35b2two"
 export LAPACK_ROOT="/opt/spack/opt/spack/linux-ubuntu22.04-x86_64_v3/gcc-11.4.0/openblas-0.3.23-bwv7xuj5t72zlgxhiq4wz3nyb35b2two"
 
 cmake -G "${CMAKE_GENERATOR:-Ninja}" \
     -D CMAKE_PREFIX_PATH="/opt/spack/opt/spack/linux-ubuntu22.04-x86_64_v3/gcc-11.4.0" \
+    -D CMAKE_BUILD_TYPE=DEBUG \
+    -D Trilinos_ENABLE_DEBUG=ON \
     -D Trilinos_PARALLEL_LINK_JOBS_LIMIT=2 \
     -D Trilinos_ENABLE_ALL_PACKAGES=ON \
     -D Trilinos_ENABLE_ALL_OPTIONAL_PACKAGES=ON \
@@ -28,15 +32,13 @@ cmake -G "${CMAKE_GENERATOR:-Ninja}" \
     -D Trilinos_ENABLE_TESTS=ON \
     -D Trilinos_TEST_CATEGORIES=BASIC \
     -D Trilinos_ENABLE_ALL_FORWARD_DEP_PACKAGES=ON \
+    -D Trilinos_VERBOSE_CONFIGURE=ON \
+    -D BUILD_SHARED_LIBS:BOOL=ON \
     \
     -D TPL_ENABLE_BLAS=ON \
-    -D BLAS_LIBRARY_DIRS='${BLAS_ROOT}/lib' \
-    -D TPL_BLAS_LIBRARIES='${BLAS_ROOT}/lib/blas;${BLAS_ROOT}/lib/blas_win32' \
-    -D BLAS_LIBRARY_NAMES="blas blas_win32" \
+    -D BLAS_LIBRARY_DIRS="${BLAS_ROOT}/lib" \
     -D TPL_ENABLE_LAPACK=ON \
     -D LAPACK_LIBRARY_DIRS="${LAPACK_ROOT}/lib" \
-    -D LAPACK_LIBRARY_NAMES="lapack lapack_win32" \
-    -D LAPACK_LIB_DEFINED_DEPENDENCIES='BLAS' \
     \
     -D TPL_ENABLE_Matio=OFF \
     -D TPL_ENABLE_X11=OFF \
