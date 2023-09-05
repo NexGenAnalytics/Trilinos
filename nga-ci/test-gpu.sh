6ce83a6ca0f06e965df03a59b6992942a5390565
Thomas Dutheillet-Lamonthézie <thomas.dutheillet@ng-analytics.com>
2023-09-05 11:46:53 +0200

    #128: fix artifacts missing

--- nga-ci/test-gpu.sh
+++ nga-ci/test-gpu.sh
@@ -6,17 +6,19 @@ set -e
 . /opt/spack/share/spack/setup-env.sh
 spack env activate trilinos
 
-pushd /opt/build/Trilinos
+cd /opt/build/Trilinos
 ret_code=0
 
 export OMPI_ALLOW_RUN_AS_ROOT=1
 export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
 
+# path to the artifacts
+artifacts_dir=/opt/build/Trilinos/artifacts/
+
 ctest -j 5 --output-junit junit-tests-report.xml --output-on-failure || ret_code=$?
 # We collect the test logs for exporting
 echo "ctest returned: $ret_code"
-mkdir -p /tmp/artifacts/
-cp /opt/build/Trilinos/Testing/Temporary/LastTest.log /tmp/artifacts/
-echo ${ret_code} > /tmp/artifacts/success_flag.txt
-ls /tmp/artifacts
-popd
+mkdir -p ${artifacts_dir}
+cp /opt/build/Trilinos/Testing/Temporary/LastTest.log ${artifacts_dir}
+echo ${ret_code} > ${artifacts_dir}/success_flag.txt
+ls ${artifacts_dir}
