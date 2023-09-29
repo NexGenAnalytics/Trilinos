b51539f7af48cfe5be4e01c1637fcdfb79451ded
Thomas Dutheillet-Lamonthézie <thomas.dutheillet@ng-analytics.com>
2023-09-29 16:25:40 +0200

    #197: update FiniteElementProblem class

--- packages/nox/test/tpetra/LOCA_TestProblems/FiniteElementProblem.cpp
+++ packages/nox/test/tpetra/LOCA_TestProblems/FiniteElementProblem.cpp
@@ -48,61 +48,58 @@
 #include <NOX_Common.H>
 
 #include "Basis.hpp"
+
 #include "FiniteElementProblem.hpp"
 
 // Constructor - creates the Tpetra objects (maps and vectors)
-template typename<ST>
+template <typename Scalar>
 FiniteElementProblem<ST>::FiniteElementProblem(int numGlobalElements,
-                       Teuchos::RCP<const Teuchos::Comm<int>> &comm,
-                       ST s) :
-  Comm(&comm),
-  NumGlobalElements(numGlobalElements),
-  scale(s)
-{
-
+                                               Teuchos::RCP<const Teuchos::Comm<int>>& comm,
+                                               Scalar s)
+    : comm(&comm), numGlobalElements(numGlobalElements), scale(s) {
   // Commonly used variables
   int i;
-  MyPID = Comm->getRank();      // Process ID
-  NumProc = Comm->getSize();  // Total number of processes
+  myRank = comm->getRank();   // Process ID
+  numProc = comm->getSize();  // Total number of processes
 
   // Construct a Source Map that puts approximately the same
   // Number of equations on each processor in uniform global ordering
-  StandardMap = new tmap_t(NumGlobalElements, 0, *Comm);
+  standardMap = new map_type(numGlobalElements, 0, *comm);
 
   // Get the number of elements owned by this processor
-  NumMyElements = StandardMap->getLocalNumElements();
+  numMyElements = standardMap->getLocalNumElements();
 
   // Construct an overlaped map for the finite element fill *************
   // For single processor jobs, the overlap and standard map are the same
-  if (NumProc == 1) {
-    OverlapMap = new tmap_t(*StandardMap);
+  if (numProc == 1) {
+    overlapMap = new map_type(*standardMap);
   } else {
+    int overlapNumMyElements;
+    int overlapMinMyGID;
+    overlapNumMyElements = numMyElements + 2;
+    if ((myRank == 0) || (myRank == numProc - 1))
+      overlapNumMyElements--;
 
-    int OverlapNumMyElements;
-    int OverlapMinMyGID;
-    OverlapNumMyElements = NumMyElements + 2;
-    if ((MyPID == 0) || (MyPID == NumProc - 1))
-      OverlapNumMyElements --;
+    if (myRank == 0) {
+      overlapMinMyGID = standardMap->getMinGlobalIndex();
+    } else {
+      overlapMinMyGID = standardMap->getMinGlobalIndex() - 1;
+    }
 
-    if (MyPID==0)
-      OverlapMinMyGID = StandardMap->getMinGlobalIndex();
-    else
-      OverlapMinMyGID = StandardMap->getMinGlobalIndex() - 1;
+    int* overlapMyGlobalElements = new int[overlapNumMyElements];
 
-    int* OverlapMyGlobalElements = new int[OverlapNumMyElements];
+    for (i = 0; i < overlapNumMyElements; i++) {
+      overlapMyGlobalElements[i] = overlapMinMyGID + i;
+    }
 
-    for (i = 0; i < OverlapNumMyElements; i ++)
-      OverlapMyGlobalElements[i] = OverlapMinMyGID + i;
+    overlapMap = new tmap_t(-1, overlapNumMyElements, overlapMyGlobalElements, 0, *comm);
 
-    OverlapMap = new tmap_t(-1, OverlapNumMyElements,
-                OverlapMyGlobalElements, 0, *Comm);
-
-  } // End Overlap map construction *************************************
+  }  // End Overlap map construction *************************************
 
   // Construct Linear Objects
-  Importer = new timport_t(*OverlapMap, *StandardMap);
-  initialSolution = new tvector_t(*StandardMap);
-  AA = new tcrsgraph_t(Copy, *StandardMap, 5);
+  importer = new timport_t(*overlapMap, *standardMap);
+  initialSolution = new tvector_t(*standardMap);
+  AA = new tcrsgraph_t(Copy, *standardMap, 5);
 
   // Allocate the memory for a matrix dynamically (i.e. the graph is dynamic).
   generateGraph(*AA);
@@ -120,41 +117,40 @@ FiniteElementProblem<ST>::FiniteElementProblem(int numGlobalElements,
 }
 
 // Matrix and Residual Fills
-template typename<ST>
-bool FiniteElementProblem::evaluate(FillType f,
-                    const tvector_t* soln,
-                    tvector_t* tmp_rhs,
-                    trowmatrix_t* tmp_matrix)
-{
+template <typename Scalar>
+bool FiniteElementProblem::evaluate(FillType f, const tvector_t* soln, tvector_t* tmp_rhs,
+                                    trowmatrix_t* tmp_matrix) {
   flag = f;
 
   // Set the incoming linear objects
   if (flag == F_ONLY) {
     rhs = tmp_rhs;
   } else if (flag == MATRIX_ONLY) {
-    A = dynamic_cast<tcrsmatrix_t*> (tmp_matrix);
+    A = dynamic_cast<tcrsmatrix_t*>(tmp_matrix);
   } else if (flag == ALL) {
     rhs = tmp_rhs;
-    A = dynamic_cast<tcrsmatrix_t*> (tmp_matrix);
+    A = dynamic_cast<tcrsmatrix_t*>(tmp_matrix);
   } else {
     std::cout << "ERROR: FiniteElementProblem::fillMatrix() - FillType flag is broken" << std::endl;
     throw;
   }
 
   // Create the overlapped solution and position vectors
-  tvector_t u(*OverlapMap);
-  tvector_t x(*OverlapMap);
+  vector_type u(*overlapMap);
+  vector_type x(*overlapMap);
 
   // Export Solution to Overlap vector
-  u.doImport(*soln, *Importer, Tpetra::INSERT);
+  u.doImport(*soln, *importer, Tpetra::INSERT);
 
   // Declare required variables
-  int i,j,ierr;
-  int OverlapNumMyElements = OverlapMap->NumMyElements();
+  int i, j, ierr;
+  int overlapNumMyElements = overlapMap->numMyElements();
 
-  int OverlapMinMyGID;
-  if (MyPID==0) OverlapMinMyGID = StandardMap->getMinGlobalIndex();
-  else OverlapMinMyGID = StandardMap->getMinGlobalIndex()-1;
+  int overlapMinMyGID;
+  if (myRank == 0)
+    overlapMinMyGID = standardMap->getMinGlobalIndex();
+  else
+    overlapMinMyGID = standardMap->getMinGlobalIndex() - 1;
 
   int row, column;
   ST jac;
@@ -163,152 +159,147 @@ bool FiniteElementProblem::evaluate(FillType f,
   Basis basis;
 
   // Create the nodal coordinates
-  ST Length=1.0;
-  ST dx=Length/((ST) NumGlobalElements-1);
-  for (i=0; i < OverlapNumMyElements; i++) {
-    x[i]=dx*((ST) OverlapMinMyGID+i);
+  ST Length = 1.0;
+  ST dx = Length / ((ST)numGlobalElements - 1);
+  for (i = 0; i < overlapNumMyElements; i++) {
+    x[i] = dx * ((ST)overlapMinMyGID + i);
   }
 
   // Zero out the objects that will be filled
-  if ((flag == MATRIX_ONLY) || (flag == ALL)) i=A->PutScalar(0.0);
-  if ((flag == F_ONLY)    || (flag == ALL)) i=rhs->PutScalar(0.0);
+  if ((flag == MATRIX_ONLY) || (flag == ALL))
+    i = A->PutScalar(0.0);
+  if ((flag == F_ONLY) || (flag == ALL))
+    i = rhs->PutScalar(0.0);
 
   // Loop Over # of Finite Elements on Processor
-  for (int ne=0; ne < OverlapNumMyElements-1; ne++) {
-
+  for (int ne = 0; ne < overlapNumMyElements - 1; ne++) {
     // Loop Over Gauss Points
-    for(int gp=0; gp < 2; gp++) {
+    for (int gp = 0; gp < 2; gp++) {
       // Get the solution and coordinates at the nodes
-      xx[0]=x[ne];
-      xx[1]=x[ne+1];
-      uu[0]=u[ne];
-      uu[1]=u[ne+1];
+      xx[0] = x[ne];
+      xx[1] = x[ne + 1];
+      uu[0] = u[ne];
+      uu[1] = u[ne + 1];
       // Calculate the basis function at the gauss point
       basis.getBasis(gp, xx, uu);
 
       // Loop over Nodes in Element
-      for (i=0; i< 2; i++) {
-    row=OverlapMap->GID(ne+i);
-    //printf("Proc=%d GlobalRow=%d LocalRow=%d Owned=%d\n",
-    //     MyPID, row, ne+i,StandardMap.MyGID(row));
-    if (StandardMap->MyGID(row)) {
-      if ((flag == F_ONLY)    || (flag == ALL)) {
-        (*rhs)[StandardMap->LID(OverlapMap->GID(ne+i))]+=
-          +basis.wt*basis.dx
-          *((-1.0/(basis.dx*basis.dx))*basis.duu*
-        basis.dphide[i]+factor*basis.uu*basis.uu*basis.uu*basis.phi[i]);
-      }
-    }
-    // Loop over Trial Functions
-    if ((flag == MATRIX_ONLY) || (flag == ALL)) {
-      for(j=0;j < 2; j++) {
-        if (StandardMap->MyGID(row)) {
-          column=OverlapMap->GID(ne+j);
-          jac=basis.wt*basis.dx*((-1.0/(basis.dx*basis.dx))*
-                     basis.dphide[j]*basis.dphide[i]
-                     +3.0*factor*basis.uu*basis.uu*basis.phi[j]*
-                     basis.phi[i]);
-          ierr=A->sumIntoGlobalValues(row, 1, &jac, &column);
+      for (i = 0; i < 2; i++) {
+        row = overlapMap->getGlobalElement(ne + i);
+        // printf("Proc=%d GlobalRow=%d LocalRow=%d Owned=%d\n",
+        //      myRank, row, ne+i,standardMap.MyGID(row));
+        if (standardMap->MyGID(row)) {
+          if ((flag == F_ONLY) || (flag == ALL)) {
+            (*rhs)[standardMap->LID(overlapMap->GID(ne + i))] +=
+                +basis.wt * basis.dx *
+                ((-1.0 / (basis.dx * basis.dx)) * basis.duu * basis.dphide[i] +
+                 factor * basis.uu * basis.uu * basis.uu * basis.phi[i]);
+          }
         }
-      }
-    }
+        // Loop over Trial Functions
+        if ((flag == MATRIX_ONLY) || (flag == ALL)) {
+          for (j = 0; j < 2; j++) {
+            if (standardMap->getLocalElement(row)) {
+              column = overlapMap->GID(ne + j);
+              jac = basis.wt * basis.dx *
+                    ((-1.0 / (basis.dx * basis.dx)) * basis.dphide[j] * basis.dphide[i] +
+                     3.0 * factor * basis.uu * basis.uu * basis.phi[j] * basis.phi[i]);
+              ierr = A->sumIntoGlobalValues(row, 1, &jac, &column);
+            }
+          }
+        }
       }
     }
   }
 
   // Insert Boundary Conditions and modify Jacobian and function (F)
   // U(0)=1
-  if (MyPID==0) {
-    if ((flag == F_ONLY)    || (flag == ALL))
-      (*rhs)[0]= (*soln)[0] - leftBC;
+  if (myRank == 0) {
+    if ((flag == F_ONLY) || (flag == ALL))
+      (*rhs)[0] = (*soln)[0] - leftBC;
     if ((flag == MATRIX_ONLY) || (flag == ALL)) {
-      column=0;
-      jac=1.0;
+      column = 0;
+      jac = 1.0;
       A->replaceGlobalValues(0, 1, &jac, &column);
-      column=1;
-      jac=0.0;
+      column = 1;
+      jac = 0.0;
       A->replaceGlobalValues(0, 1, &jac, &column);
     }
   }
 
-  if ( StandardMap->LID(StandardMap->getMaxAllGlobalIndex()) >= 0 ) {
-    int lastDof = StandardMap->LID(StandardMap->getMaxAllGlobalIndex());
-    if ((flag == F_ONLY)    || (flag == ALL))
-       (*rhs)[lastDof] = (*soln)[lastDof] - rightBC;
+  if (standardMap->LID(standardMap->getMaxAllGlobalIndex()) >= 0) {
+    int lastDof = standardMap->getLocalElement(standardMap->getMaxAllGlobalIndex());
+    if ((flag == F_ONLY) || (flag == ALL))
+      (*rhs)[lastDof] = (*soln)[lastDof] - rightBC;
     if ((flag == MATRIX_ONLY) || (flag == ALL)) {
-       int row = StandardMap->getMaxAllGlobalIndex();
-       column = row;
-      jac=1.0;
+      int row = standardMap->getMaxAllGlobalIndex();
+      column = row;
+      jac = 1.0;
       A->replaceGlobalValues(row, 1, &jac, &column);
-      column=row-1;
-      jac=0.0;
+      column = row - 1;
+      jac = 0.0;
       A->replaceGlobalValues(row, 1, &jac, &column);
     }
   }
   // Sync up processors to be safe
-  Comm->barrier();
+  comm->barrier();
 
   A->fillComplete();
 
   return true;
 }
 
-template typename<ST>
-Teuchos::RCP<tvector_t> FiniteElementProblem::getSolution()
-{
+template <typename Scalar>
+Teuchos::RCP<tvector_t> FiniteElementProblem::getSolution() {
   return initialSolution;
 }
 
-template typename<ST>
-Teuchos::RCP<tcrsmatrix_t> FiniteElementProblem::getJacobian()
-{
+template <typename Scalar>
+Teuchos::RCP<tcrsmatrix_t> FiniteElementProblem::getJacobian() {
   return A;
 }
 
-template typename<ST>
-bool FiniteElementProblem::setParameter(std::string label, ST value)
-{
+template <typename Scalar>
+bool FiniteElementProblem::setParameter(std::string label, ST value) {
   if (label == "Nonlinear Factor")
     factor = value;
   else if (label == "Left BC")
     leftBC = value;
   else if (label == "Right BC")
-    rightBC = value*scale;
+    rightBC = value * scale;
   else if (label == "Homotopy Continuation Parameter") {
     // do nothing for now
-  }
-  else {
+  } else {
     std::cout << "ERROR: FiniteElementProblem::setParameter() - label is invalid "
-     << "for this problem!" << std::endl;
+              << "for this problem!" << std::endl;
     exit(-1);
   }
   return true;
 }
 
-template typename<ST>
-tcrsgraph_t& FiniteElementProblem::generateGraph(tcrsgraph_t& AAA)
-{
+template <typename Scalar>
+tcrsgraph_t& FiniteElementProblem::generateGraph(tcrsgraph_t& AAA) {
   // Declare required variables
-  int i,j;
+  int i, j;
   int row, column;
-  int OverlapNumMyElements = OverlapMap->getLocalNumElements();
-  int OverlapMinMyGID;
-  if (MyPID==0) OverlapMinMyGID = StandardMap->getMinGlobalIndex();
-  else OverlapMinMyGID = StandardMap->getMinGlobalIndex()-1;
+  int overlapNumMyElements = overlapMap->getLocalNumElements();
+  int overlapMinMyGID;
+  if (myRank == 0)
+    overlapMinMyGID = standardMap->getMinGlobalIndex();
+  else
+    overlapMinMyGID = standardMap->getMinGlobalIndex() - 1;
 
   // Loop Over # of Finite Elements on Processor
-  for (int ne=0; ne < OverlapNumMyElements-1; ne++) {
-
+  for (int ne = 0; ne < overlapNumMyElements - 1; ne++) {
     // Loop over Nodes in Element
-    for (i=0; i< 2; i++) {
-      row=OverlapMap->GID(ne+i);
+    for (i = 0; i < 2; i++) {
+      row = overlapMap->GID(ne + i);
 
       // Loop over Trial Functions
-      for(j=0;j < 2; j++) {
-        
+      for (j = 0; j < 2; j++) {
         // If this row is owned by current processor, add the index
-        if (StandardMap->MyGID(row)) {
-          column=OverlapMap->GID(ne+j);
+        if (standardMap->MyGID(row)) {
+          column = overlapMap->GID(ne + j);
           AAA.insertGlobalIndices(row, 1, &column);
         }
       }
